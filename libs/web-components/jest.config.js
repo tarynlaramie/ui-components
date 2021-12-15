a0e1624933d1a8c738e1449182888efc4c5ea5ad
Taryn Laramie <taryn.laramie@gov.ab.ca>
2021-12-15 13:26:46 -0700

    add web-components lib

---
+++ libs/web-components/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'web-components',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  transform: {
+    '^(.+\\.svelte$)': 'svelte-jester',
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['svelte', 'ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/web-components',
+};
